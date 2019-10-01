2ffd2919c7f7f0c12996db683ac2df9308a0f079
Yerko Cuzmar <yerko.cuzmar.14@sansano.usm.cl>
2019-09-30 22:34:02 -0300

    cambios en el manejo de archivo de log del servidor.

--- actividad1/app_server/server.py
+++ actividad1/app_server/server.py
@@ -1,9 +1,10 @@
 import socket
 
+
+print('SERVIDOR INICIADO')
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('', 5000))
 server.listen(10)
-file = open("log.txt", "a+")
 
 while True:
     conn, addr = server.accept()
@@ -12,10 +13,10 @@ while True:
         data = conn.recv(4096).decode()
         if not data: break
         client_message += data
-
+        file = open("log.txt", "a+")
         file.write(str(addr[0]) + ":" + str(addr[1]) + " -> " + data + "\n")
+        file.close()
         print("Mensaje recibido y registrado correctamente.\n")
         conn.send("El servidor ha recibido satisfactoriamente su mensaje.\n".encode())
     conn.close()
     print('Se ha perdido la conexión con el cliente.\n')
-file.close()
