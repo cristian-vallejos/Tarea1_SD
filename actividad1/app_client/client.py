5e6c2b9c0b4c61e7512f7f9edb29385188216b3e
Cristian Vallejos <cristian.vallejos.14@sansano.usm.cl>
2019-10-02 14:39:35 -0300

    Actividad 1 lista
    Sólo falta recuperar la imagen de ubuntu, en lugar de usar la de python.

--- actividad1/app_client/client.py
+++ actividad1/app_client/client.py
@@ -1,19 +1,23 @@
 import socket
-import datetime
+import datetime, random
 from time import sleep
 
-print('CLIENTE INICIADO')
+messages_dict = ["Hola", "Sistemas", "Mensaje de prueba", "Funciona!", "Saludos", "Distribuidos",
+                "Hola", "Sistemas", "Mensaje de prueba", "Funciona!", "Saludos", "Distribuidos", "exit"]
+
 client_file_name = 'respuestas.txt'
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 5000))  # Esto debería depender de la IP del server.
-file = open(client_file_name, "a+")
-for i in range(3):
-    message = f"Mensaje {i}"
+
+while True:
+    message = messages_dict[random.randint(0, len(messages_dict)-1)]
     client.send(message.encode())
-    server_answer = client.recv(4096)
+    server_answer = client.recv(4096).decode()
     date_now = datetime.datetime.now()
-    print(server_answer.decode())
-    file.write(date_now.strftime("%Y-%m-%d %H:%M:%S") + " -> " + server_answer.decode())
-    sleep(5)
-file.close()
+    print(server_answer)
+    with open(client_file_name, "a+") as file:
+        file.write(date_now.strftime("%Y-%m-%d %H:%M:%S") + " -> " + server_answer + "\n")
+    if message == "exit":
+        print("Se ha cerrado con éxito la conexión.")
+        break
 client.close()
--- actividad1/app_server/server.py
+++ actividad1/app_server/server.py
@@ -1,23 +1,23 @@
 import socket
 
-
-print('SERVIDOR INICIADO')
 server_file_name = 'log.txt'
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('', 5000))
 server.listen(10)
+print("Servidor iniciado.")
+print("Esperando consultas por parte del cliente...\n")
 
 while True:
     conn, addr = server.accept()
-    client_message = ''
     while True:
-        data = conn.recv(4096).decode()
+        data = conn.recv(4096)
         if not data: break
-        client_message += data
+        print("Se ha establecido conexión con el cliente.\n")
         file = open(server_file_name, "a+")
-        file.write(str(addr[0]) + ":" + str(addr[1]) + " -> " + data + "\n")
+        file.write(str(addr[0]) + ":" + str(addr[1]) + " -> " + data.decode() + "\n")
         file.close()
         print("Mensaje recibido y registrado correctamente.\n")
-        conn.send("El servidor ha recibido satisfactoriamente su mensaje.\n".encode())
-    conn.close()
+        conn.send(("El servidor ha recibido satisfactoriamente su mensaje '" + data.decode() + "'.\n").encode())
     print('Se ha perdido la conexión con el cliente.\n')
+    conn.close()
+server.close()
