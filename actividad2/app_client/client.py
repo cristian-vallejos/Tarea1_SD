9708d7116b54a41c7bf4f8f236996bb9b0299afe
Yerko Cuzmar <yerko.cuzmar.14@sansano.usm.cl>
2019-10-02 13:34:51 -0300

    Act. 2: Mensajes automaticos de cliente.

--- actividad2/app_client/client.py
+++ actividad2/app_client/client.py
@@ -4,16 +4,13 @@ import datetime
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(("127.0.0.1", 5000)) # Esto debería depender de la IP del server.
 
-while True:
-    message = input("Ingrese el mensaje a enviar (pulse ENTER para cerrar el cliente): ")
-    if message == "":
-        client.send(("close").encode())
-        break
-    else:
-        client.send(("client | " + message).encode())
-        server_answer = client.recv(2048).decode()
-        print(server_answer)
-        date_now = datetime.datetime.now()
-        with open("registro_cliente.txt", "a+") as file:
-            file.write(date_now.strftime("%Y-%m-%d %H:%M:%S") + " -> " + server_answer + "\n")
+for i in range(3):
+    message = f"Mensaje cliente {i}"
+    client.send(("client | " + message).encode())
+    server_answer = client.recv(2048).decode()
+    print(server_answer)
+    date_now = datetime.datetime.now()
+    with open("registro_cliente.txt", "a+") as file:
+        file.write(date_now.strftime("%Y-%m-%d %H:%M:%S") + " -> " + server_answer + "\n")
+client.send(("close").encode())
 client.close()
