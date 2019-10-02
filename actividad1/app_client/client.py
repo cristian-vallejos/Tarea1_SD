9eb3e69d3e5f681dfefd7840f121fc81a8206961
Yerko Cuzmar <yerko.cuzmar.14@sansano.usm.cl>
2019-10-01 23:13:11 -0300

    Se quita el user input del cliente y se mandan mensajes predefinidos al servidor.

--- actividad1/app_client/client.py
+++ actividad1/app_client/client.py
@@ -1,21 +1,24 @@
 import socket
 import datetime
+from time import sleep
 
 print('CLIENTE INICIADO')
+client_file_name = 'respuestas.txt'
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 5000))  # Esto debería depender de la IP del server.
-file = open("respuestas.txt", "a+")
-
-while True:
-    message = input("Ingrese el mensaje a enviar (pulse ENTER para cerrar el cliente): ")
-    print('MSG INPUT')
-    if message == "":
-        break
-    else:
-        client.send(message.encode())
-        server_answer = client.recv(4096)
-        date_now = datetime.datetime.now()
-        print(server_answer.decode())
-        file.write(date_now.strftime("%Y-%m-%d %H:%M:%S") + " -> " + server_answer.decode() + "\n")
+file = open(client_file_name, "a+")
+for i in range(10):
+    message = f"Mensaje {i}"
+    client.send(message.encode())
+    server_answer = client.recv(4096)
+    date_now = datetime.datetime.now()
+    print(server_answer.decode())
+    file.write(date_now.strftime("%Y-%m-%d %H:%M:%S") + " -> " + server_answer.decode())
+    sleep(1)
 file.close()
 client.close()
+print(f"{client_file_name} CONTENT")
+file = open(client_file_name, 'r')
+for line in file:
+    print(line)
+file.close()
--- actividad1/app_server/server.py
+++ actividad1/app_server/server.py
@@ -2,6 +2,7 @@ import socket
 
 
 print('SERVIDOR INICIADO')
+server_file_name = 'log.txt'
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('', 5000))
 server.listen(10)
@@ -13,10 +14,15 @@ while True:
         data = conn.recv(4096).decode()
         if not data: break
         client_message += data
-        file = open("log.txt", "a+")
+        file = open(server_file_name, "a+")
         file.write(str(addr[0]) + ":" + str(addr[1]) + " -> " + data + "\n")
         file.close()
         print("Mensaje recibido y registrado correctamente.\n")
         conn.send("El servidor ha recibido satisfactoriamente su mensaje.\n".encode())
     conn.close()
     print('Se ha perdido la conexión con el cliente.\n')
+    file = open(server_file_name, 'r')
+    print('LOG FILE:')
+    for line in file:
+        print(line)
+    file.close()
